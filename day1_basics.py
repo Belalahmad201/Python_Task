35878bc12d2af01a8ef2126517f6e764372a04ec
Belal Ahmad <ahmadbelal802211@gmail.com>
2026-05-17 20:16:35 +0530

    Added day1 basics challenge

---
+++ day1_basics.py
@@ -0,0 +1,12 @@
+# Take user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Convert age into integer
+age = int(age)
+
+# Print formatted message
+print(f"Hello {name}, you are {age} years old.")
+
+# Print age after 5 years
+print(f"In 5 years, you will be {age + 5}.")
